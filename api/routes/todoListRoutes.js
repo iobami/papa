b12838a9863c9a870379c3c889cdab39b0588030
iobami <ayobamialadenoye@yahoo.com>
2020-03-28 13:42:28 +0100

    first commit

--- api/routes/todoListRoutes.js
+++ api/routes/todoListRoutes.js
@@ -3,12 +3,12 @@ module.exports = function(app) {
     const todoList = require('../controllers/todoListController');
 
     // todoList Routes
-    app.route('/tasks')
+    app.route('/api/v1/images')
         .get(todoList.list_all_tasks)
         .post(todoList.create_a_task);
 
 
-    app.route('/tasks/:id')
+    app.route('/api/v1/images/:id')
         .get(todoList.read_a_task)
         .put(todoList.update_a_task)
         .delete(todoList.delete_a_task);
